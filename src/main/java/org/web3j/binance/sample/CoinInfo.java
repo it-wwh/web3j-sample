ab59884822bd7dbb7764ef7d0f9b2ca62be0f095
honghui <wanghonghui_work@163.com>
2022-04-22 15:46:05 +0800

    addOn:本地需要设置代理才能访问

--- src/main/java/org/web3j/binance/sample/CoinInfo.java
+++ src/main/java/org/web3j/binance/sample/CoinInfo.java
@@ -10,6 +10,8 @@ import java.util.LinkedHashMap;
 public class CoinInfo {
     private static final Logger logger = LoggerFactory.getLogger(CoinInfo.class);
     public static void main(String[] args) {
+        System.setProperty("https.proxyHost", "127.0.0.1");
+        System.setProperty("https.proxyPort", "7890");
         LinkedHashMap<String,Object> parameters = new LinkedHashMap<>();
 
         SpotClientImpl client = new SpotClientImpl(Environment.BINANCE_API_KEY, Environment.BINANCE_SECRET_KEY);
